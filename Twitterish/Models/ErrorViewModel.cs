2e4d6818abdaaa636bec51fef0f02068064571e8
drenski666 <drenski666@hotmail.com>
2018-06-10 18:30:03 +0300

    Add project files.

---
+++ Twitterish/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Twitterish.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
